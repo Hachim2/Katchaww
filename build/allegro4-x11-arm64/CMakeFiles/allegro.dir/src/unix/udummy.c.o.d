3672bb67de09525246c31987e78cbfd3d90ac864
Thibault Lefort <stratus52.lefort@gmail.com>
2026-03-22 16:43:16 +0100

    Stop tracking generated files

--- build/allegro4-x11-arm64/CMakeFiles/allegro.dir/src/unix/udummy.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/allegro.dir/src/unix/udummy.c.o: \
-  /Users/emmanuellefort/Desktop/ECOLE/ECE/Informatique1/projets2/third_party/allegro4/src/unix/udummy.c
--- build/allegro4/CMakeFiles/alleg-main.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for alleg-main.
-# This may be replaced when dependencies are built.
