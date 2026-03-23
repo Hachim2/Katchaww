7b37afce09a568fa324b61adee83e2f79334a566
Thibault Lefort <stratus52.lefort@gmail.com>
2026-03-23 14:59:57 +0100

    Refactor code structure for improved readability and maintainability

--- scr/main.c
+++ scr/main.c
@@ -3,11 +3,22 @@
 #endif
 
 #include <allegro.h>
+#include <unistd.h>
 
 #define APP_WIDTH 1200
 #define APP_HEIGHT 800
 
 volatile int close_button_pressed = FALSE;
+
+static BITMAP *load_background(char* path){
+    BITMAP *bmp = load_bitmap(path, NULL);
+    return bmp;
+}
+
+static int background_exists(char* path)
+{
+    return exists(path);
+}
 
 void close_button_handler(void){
     close_button_pressed = TRUE;
@@ -17,6 +28,10 @@ END_OF_FUNCTION(close_button_handler);
 
 int main(void){
     float pos = 0;
+    char* path = "assets/bgs/bg1.bmp";
+    BITMAP *background;
+    BITMAP *background_scaled;
+    BITMAP *buffer;
 
     allegro_init();
 
@@ -34,32 +49,52 @@ int main(void){
         return 1;
     }
 
-    clear_to_color(screen, makecol(24, 28, 36));
-    textout_centre_ex(screen, font, "Allegro 4 test app", APP_WIDTH / 2, 140,
-                      makecol(255, 255, 255), -1);
-    textout_centre_ex(screen, font, "If you can read this, graphics work.",
-                      APP_WIDTH / 2, 220, makecol(200, 220, 255), -1);
-    textout_centre_ex(screen, font, "Using a fixed 800x600 window.",
-                      APP_WIDTH / 2, 260, makecol(180, 200, 235), -1);
-    textout_centre_ex(screen, font, "Press ESC to quit.", APP_WIDTH / 2, 320,
-                      makecol(255, 214, 102), -1);
-    show_mouse(screen);
+    background = load_background(path);
+    if (!background) {
+        char cwd[1024];
+        cwd[0] = '\0';
+        getcwd(cwd, sizeof(cwd));
+        allegro_message("Unable to load image from %s\ncwd: %s\nexists: %s\n%s\n",
+                        path,
+                        cwd[0] ? cwd : "(unknown)",
+                        background_exists(path) ? "yes" : "no",
+                        allegro_error);
+        return 1;
+    }
+
+    background_scaled = create_bitmap(APP_WIDTH, APP_HEIGHT);
+    buffer = create_bitmap(APP_WIDTH, APP_HEIGHT);
+    if (!background_scaled || !buffer) {
+        allegro_message("Unable to create render buffers.\n%s\n", allegro_error);
+        return 1;
+    }
+
+    stretch_blit(background, background_scaled, 0, 0, background->w, background->h,
+                 0, 0, APP_WIDTH, APP_HEIGHT);
+
 
     while (!close_button_pressed) {
-        clear_to_color(screen, makecol(24, 28, 36));
+        blit(background_scaled, buffer, 0, 0, 0, 0, APP_WIDTH, APP_HEIGHT);
 
         pos = (pos + 0.01);
         if (pos > 1) {
             pos = 0;
         }
 
+        circlefill(buffer, APP_WIDTH * pos, APP_HEIGHT * pos, 10, makecol(255, 255, 255));
+        vsync();
+        blit(buffer, screen, 0, 0, 0, 0, APP_WIDTH, APP_HEIGHT);
         show_mouse(screen);
-        circlefill(screen, APP_WIDTH * pos, APP_HEIGHT * pos, 10, makecol(255, 255, 255));
+
         if (key[KEY_DEL_PAD] || key[KEY_DEL]) {
             close_button_pressed = 1;
         }
-        rest(16);
+        rest(8);
     }
+
+    destroy_bitmap(buffer);
+    destroy_bitmap(background_scaled);
+    destroy_bitmap(background);
 
     return 0;
 }
